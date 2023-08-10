0db6be408c470570aa121e602224275e426093ab
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-11 07:23:39 +1000

    Create KitSignatures.php
    Waiting on Llouise76 to tell me what kits plugin she uses so i can make it

---
+++ src/skyss0fly/KitSignatures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace skyss0fly\KitSignatures;
+
+use pocketmine\{plugin\PluginBase,event\Listener};
+/*
+use kit
+*/
+
+class Main extends PluginBase implements Listener{
+
+public function Checker(): {
+  $a = 1 
+while $a == 1 do
+  
+
+}
+
+
+
+}
